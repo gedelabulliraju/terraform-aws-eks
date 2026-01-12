9acff9b52b3e30e78484d67c5068e3d9eb4f25da
Bulliraju <bulliraju118@gmail.com>
2026-01-12 19:03:57 +0530

    I added tf files to create eks by terraform

---
+++ 50-acm/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 6.0.0"
+    }
+  }
+
+ backend "s3" {
+    bucket         = "srlaf-remote-state1"
+    key            = "expense-infra-eks-acm"
+    region         = "us-east-1"
+    dynamodb_table = "81s-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
